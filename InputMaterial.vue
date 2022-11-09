7563302b4fc756f15f5e163e93c04903b29f133d
Denis Beliaev <cimmwolf@gmail.com>
2022-11-09 17:14:30 +0400

    Убрано создание атрибута style с CSS переменной

--- InputMaterial.vue
+++ InputMaterial.vue
@@ -59,9 +59,6 @@ export default {
     }
   },
   mounted () {
-    let color = window.getComputedStyle(this.$el).getPropertyValue('color')
-    this.$el.style.setProperty('--autofill-color', color)
-
     if (this.type === 'date') {
       let calendarIcon = `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="${color}" d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"/></svg>')`
       this.$el.style.setProperty('--calendar-icon', calendarIcon)
