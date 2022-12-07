6e62de45a8b14bfe93251a16eee1a7a1ee63c618
Denis Beliaev <cimmwolf@gmail.com>
2022-12-07 12:16:01 +0400

    Добавлен компонент InputSelectMaterial

---
+++ InputSelectMaterial.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="material-input">
+    <select :id="id"
+            ref="select"
+            :autofocus="autofocus"
+            :class="computeClass"
+            :disabled="disabled"
+            :multiple="multiple"
+            :name="name"
+            :required="required"
+            :size="size"
+            :value="valueInternal"
+            @blur="onBlur"
+            @input="onInput"
+            @invalid="onInvalid">
+      <slot/>
+    </select>
+    <label :for="id" class="control-label">{{ label }}</label>
+    <i class="bar"></i>
+  </div>
+</template>
+
+<style scoped>
+select {
+  width: 100%;
+  height: 2.1em;
+  padding: 0.125em 0 0.0625em;
+  background: none;
+  border: none;
+  line-height: 1.6;
+  box-shadow: none;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+}
+</style>
+
+<script>
+import InputMixin from './input-mixin.js'
+
+export default {
+  name: 'InputSelectMaterial',
+  mixins: [InputMixin],
+  props: {
+    multiple: Boolean,
+    size: String
+  },
+  mounted () {
+    if (this.$refs.select.children.length > 0) {
+      this.valueInternal = this.$refs.select.children[0].value
+      this.$emit('input', this.valueInternal)
+    }
+  },
+  methods: {
+    onBlur: function (e) {
+      this.validate(e.target)
+    },
+    onInput: function (e) {
+      this.$emit('update:modelValue', e.target.value)
+    }
+  },
+}
+</script>
